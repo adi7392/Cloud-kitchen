f541b256688059c1221cfaef9c9b7d5b0ce61677
adi7392 <adiiigupta7392@gmail.com>
2026-07-29 12:14:45 +0530

    project

--- project/backend/index.js
+++ project/backend/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import connectDB from "./config/db.config.js";
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 import dotenv from "dotenv";
+import authController from "./controller/auth.controller.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
 
-app.listen();
+app.use("/api/v1/auth",authController)
+
+app.listen(PORT,()=>{
+    console.log("server is running at 3000")
+});
---
+++ project/backend/middleware/verifyToken.middle.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import dotenv, { config } from "dotenv";
+
+dotenv.config()
+
+const verificationToken = (req,res,next)=>{
+    
+}
