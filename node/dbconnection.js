da6d1e6875d2b8f0580b7274a41e523ca5d18cbc
amangg1029 <amangg1029@gmail.com>
2026-08-09 21:18:58 +0530

    Removed old dir

--- node/dbconnection.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-async function connectDB() {
-    await mongoose.connect("mongodb+srv://adi7392:Adi7392@mern.lwidnej.mongodb.net/",);
-    console.log("mongodb connected...");
-}
-export default connectDB;
--- project/backend/config/rateLimit.config.js
+++
@@ -1,9 +0,0 @@
-import { rateLimit } from "express-rate-limit";
-
-const loginLimitter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
-  message: "Too many login attempts, please try again after 15 minutes",
-});
-
-export default loginLimitter;
